e68c81d7890f5339ffa6770af6073a5b9aa33d50
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-11 14:45:15 +0900

    작업

---
+++ src/main/java/com/ll/exam/tossapp/app/member/exception/AlreadyJoinException.java
@@ -0,0 +1,4 @@
+package com.ll.exam.tossapp.app.member.exception;
+
+public class AlreadyJoinException extends RuntimeException {
+}
---
+++ src/main/java/com/ll/exam/tossapp/app/member/service/MemberService.java
@@ -0,0 +1,40 @@
+package com.ll.exam.tossapp.app.member.service;
+
+import com.ll.exam.tossapp.app.member.entity.Member;
+import com.ll.exam.tossapp.app.member.exception.AlreadyJoinException;
+import com.ll.exam.tossapp.app.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Member join(String username, String password, String email) {
+        if (memberRepository.findByUsername(username).isPresent()) {
+            throw new AlreadyJoinException();
+        }
+
+        Member member = Member.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .email(email)
+                .build();
+
+        memberRepository.save(member);
+
+        return member;
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Member> findByUsername(String username) {
+        return memberRepository.findByUsername(username);
+    }
+}
