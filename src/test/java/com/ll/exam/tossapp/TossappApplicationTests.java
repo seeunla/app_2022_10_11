ec227c20aa30e42298ac3d05083c4e627fed9902
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-11 13:20:47 +0900

    세팅

---
+++ src/test/java/com/ll/exam/tossapp/TossappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.tossapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
