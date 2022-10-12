a590f6859d1e5c3a88014e4cb83629f020d75df8
seeunla <107592967+seeunla@users.noreply.github.com>
2022-10-12 13:08:30 +0900

    캐시 추가

---
+++ src/main/java/com/ll/exam/tossapp/app/cash/service/CashService.java
@@ -0,0 +1,24 @@
+package com.ll.exam.tossapp.app.cash.service;
+
+import com.ll.exam.tossapp.app.cash.entity.CashLog;
+import com.ll.exam.tossapp.app.cash.repository.CashLogRepository;
+import com.ll.exam.tossapp.app.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CashService {
+    private final CashLogRepository cashLogRepository;
+    public CashLog addCash(Member member, long price, String eventType) {
+        CashLog cashLog = CashLog.builder()
+                .member(member)
+                .price(price)
+                .eventType(eventType)
+                .build();
+
+        cashLogRepository.save(cashLog);
+
+        return cashLog;
+    }
+}
